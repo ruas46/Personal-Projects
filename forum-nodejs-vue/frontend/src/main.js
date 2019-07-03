46d91e85faf27ccffa8f9f3bd1b6cfbf174599f9
ruas46 <guilhermeruas@hotmail.com>
2019-07-03 14:37:57 -0300

    Added baseApiUrl for axios and Temporary Valid Token for development

--- forum-nodejs-vue/frontend/src/main.js
+++ forum-nodejs-vue/frontend/src/main.js
@@ -8,6 +8,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+//Temporary Valid Token to develop Front-End
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6IlRlc3RVc2VyIiwiZW1haWwiOiJ0ZXN0QHRlc3QuY29tIiwiYWRtaW4iOmZhbHNlLCJpYXQiOjE1NjIxNzQ1NjgsImV4cCI6MTU2MjQzMzc2OH0.uwcWGxm24R-w5qU08Xq_LhhCfkl5p-ICDTr90WDhcps'
+
 new Vue({
   store,
   router,
