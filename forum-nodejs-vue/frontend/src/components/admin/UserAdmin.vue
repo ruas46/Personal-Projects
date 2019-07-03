8f56da2764325e155859883c70bff4434d39c481
ruas46 <guilhermeruas@hotmail.com>
2019-07-03 17:18:17 -0300

    Added User CRUD in Admin page

--- forum-nodejs-vue/frontend/src/components/admin/UserAdmin.vue
+++ forum-nodejs-vue/frontend/src/components/admin/UserAdmin.vue
@@ -1,11 +1,72 @@
 <template>
     <div class="user-admin">
-        <b-table hover striped :items="users" :fields="fields"></b-table>
+        <b-form>
+            <input type="hidden" id="user-id" v-model="user.id" />
+            <b-row>
+                <b-col md="6" sm="12">
+                    <b-form-group label="Name:" label-for="user-name">
+                        <b-form-input id="user-name" type="text"
+                            v-model="user.name"
+                            :readonly="mode === 'remove'"
+                            placeholder="Enter the user name"
+                        />
+                    </b-form-group>
+                </b-col>
+                <b-col md="6" sm="12">
+                    <b-form-group label="E-mail:" label-for="user-email">
+                        <b-form-input id="user-email" type="text"
+                            v-model="user.email"
+                            :readonly="mode === 'remove'"
+                            placeholder="Enter the user email"
+                        />
+                    </b-form-group>
+                </b-col>
+            </b-row>
+            <b-form-checkbox v-show="mode === 'save'" id="user-admin" v-model="user.admin" class="mt-3 mb-3">
+                Administrator?
+            </b-form-checkbox>
+            <b-row v-show="mode === 'save'">
+                <b-col md="6" sm="12">
+                    <b-form-group label="Password:" label-for="user-password">
+                        <b-form-input id="user-password" type="password"
+                            v-model="user.password"
+                            placeholder="Enter the Password"
+                        />
+                    </b-form-group>
+                </b-col>
+                <b-col md="6" sm="12">
+                    <b-form-group label="Confirm:" label-for="user-confirm-password">
+                        <b-form-input id="user-confirm-password" type="password"
+                            v-model="user.confirmPassword"
+                            placeholder="Confirm Password"
+                        />
+                    </b-form-group>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col xs="12">
+                    <b-button variant="primary" v-if="mode === 'save'" @click="save">Save</b-button>
+                    <b-button variant="danger" v-if="mode === 'remove'" @click="remove">Remove</b-button>
+                    <b-button @click="reset" class="ml-2">Cancel</b-button>
+                </b-col>
+            </b-row>
+        </b-form>
+        <hr />
+        <b-table hover striped :items="users" :fields="fields">
+            <template slot="actions" slot-scope="data">
+                <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+                    <i class="fa fa-pencil"></i>
+                </b-button>
+                <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+                    <i class="fa fa-trash"></i>
+                </b-button>
+            </template>
+        </b-table>
     </div>
 </template>
 
 <script>
-import { baseApiUrl } from '@/global'
+import { baseApiUrl, showError } from '@/global'
 import axios from 'axios'
 
 export default {
@@ -32,6 +93,34 @@ export default {
             axios.get(url).then(res => {
                 this.users = res.data
             })
+        },
+        reset() {
+            this.mode = 'save'
+            this.user = {}
+            this.loadUsers()
+        },
+        save() {
+            const method = this.user.id ? 'put' : 'post'
+            const id = this.user.id ? `/${this.user.id}` : ''
+            axios[method](`${baseApiUrl}/users${id}`, this.user)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+        remove() {
+            const id = `/${this.user.id}`
+            axios.delete(`${baseApiUrl}/users${id}`)
+                .then(() => {
+                    this.$toasted.global.defaultSuccess()
+                    this.reset()
+                })
+                .catch(showError)
+        },
+        loadUser(user, mode = 'save') {
+            this.mode = mode
+            this.user = { ...user }
         }
     },
     mounted() {
