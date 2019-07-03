10269188530bad93f8b9d758dd1044180b90f1e2
ruas46 <guilhermeruas@hotmail.com>
2019-07-03 14:54:36 -0300

    Added base components Article, Category and User inside Admin Page

--- forum-nodejs-vue/frontend/src/components/admin/AdminPages.vue
+++ forum-nodejs-vue/frontend/src/components/admin/AdminPages.vue
@@ -1,15 +1,33 @@
 <template>
     <div class="admin-pages">
         <PageTitle icon="fa fa-cogs" main="Sistem Administration" sub="Configurations" />
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Articles" active>
+                        <ArticleAdmin />
+                    </b-tab>
+                    <b-tab title="Categories" active>
+                        <CategoryAdmin />
+                    </b-tab>
+                    <b-tab title="Users" active>
+                        <UserAdmin />
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import ArticleAdmin from './ArticleAdmin'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
 
 export default {
     name: 'AdminPages',
-    components: { PageTitle }
+    components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
 }
 </script>
 
---
+++ forum-nodejs-vue/frontend/src/components/admin/CategoryAdmin.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="category-admin">
+        <h1>Category COMPONENT</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CategoryAdmin'
+}
+</script>
+
+<style>
+
+</style>
