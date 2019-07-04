0bfed132ccb8d7ea1b61a2668a2989a5eb184833
ruas46 <guilhermeruas@hotmail.com>
2019-07-04 19:27:33 -0300

    Added Example to enable knex query debug

--- forum-nodejs-vue/backend/config/db.js
+++ forum-nodejs-vue/backend/config/db.js
@@ -2,4 +2,5 @@ const config = require('../knexfile.js')
 const knex = require('knex')(config)
 
 knex.migrate.latest([config])//Be Careful with this
+//knex.on('query', query => console.log(query));//Enable for query debug
 module.exports = knex
