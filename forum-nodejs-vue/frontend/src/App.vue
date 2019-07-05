4db2c864dc47ce39de2ec8c85047473a23b81639
ruas46 <guilhermeruas@hotmail.com>
2019-07-05 17:03:58 -0300

    Added gif when token validation

--- forum-nodejs-vue/frontend/src/App.vue
+++ forum-nodejs-vue/frontend/src/App.vue
@@ -5,23 +5,25 @@
 			:hideUserDropdown="!user"
 		/>
 		<Menu v-if="user" />
-		<Content />
+		<Loading v-if="validatingToken" />
+		<Content v-else />
 		<Footer />
 	</div>
 </template>
 
 <script>
 import axios from "axios"
-import {baseApiUrl, userKey } from "@/global"
+import { baseApiUrl, userKey } from "@/global"
 import { mapState } from 'vuex'
 import Header from "./components/template/Header"
 import Menu from "./components/template/Menu"
 import Content from "./components/template/Content"
 import Footer from "./components/template/Footer"
+import Loading from "./components/template/Loading"
 
 export default {
 	name: "App",
-	components: { Header, Menu, Content, Footer },
+	components: { Header, Menu, Content, Footer, Loading },
 	computed: mapState(['isMenuVisible', 'user']),
 	data: function() {
 		return {
