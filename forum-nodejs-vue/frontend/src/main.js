f1323c0035ed830cc9fd8c16b36ea36ec9b81018
ruas46 <guilhermeruas@hotmail.com>
2019-07-05 17:28:32 -0300

    Added validation with expired token

--- forum-nodejs-vue/frontend/src/main.js
+++ forum-nodejs-vue/frontend/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import './config/bootstrap'
 import './config/msgs'
+import './config/axios'
 import store from './config/store'
 import router from './config/router'
 
