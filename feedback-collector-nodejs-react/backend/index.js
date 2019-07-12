0dce1cfcc6438579cf23a9f5d1fe81d8a013120f
ruas46 <guilhermeruas@hotmail.com>
2019-07-12 14:25:31 -0300

    Refactoring the backend structure

--- feedback-collector-nodejs-react/backend/index.js
+++ feedback-collector-nodejs-react/backend/index.js
@@ -1,34 +1,8 @@
 const express = require('express')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/.keys')
+require('./services/passport')
+
 const app = express()
+require('./routes/authRoutes')(app)
+
 const PORT = process.env.PORT || 5000 //Heroku Deploy
-
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-        console.log('access Token: ', accessToken)
-        console.log('refresh Token: ', refreshToken)
-        console.log('profile: ', profile)
-        console.log('done: ', done)
-    })
-)
-
-app.get('/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
-
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'})
-})
-
 app.listen(PORT)
