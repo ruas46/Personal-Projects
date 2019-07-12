b95e78295584247b259ef6c81dbf5f9763450d5c
ruas46 <guilhermeruas@hotmail.com>
2019-07-12 17:33:19 -0300

    Added user validation on create

--- feedback-collector-nodejs-react/backend/services/passport.js
+++ feedback-collector-nodejs-react/backend/services/passport.js
@@ -12,6 +12,17 @@ passport.use(
         callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id }).save()
+        User.findOne({ googleId: profile.id })
+            .then((existingUser) => {
+                if (existingUser) {
+                    //We already have a record with the given profile ID
+                    done(null, existingUser)
+                } else {
+                    //We don't have a user record with this ID, make a new one
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user))
+                }
+            })
     })
 )
