07400106ca72cbdd780c4c3b9b6c7a76b8d81086
ruas46 <guilhermeruas@hotmail.com>
2019-07-12 19:39:01 -0300

    Added heroku involvement configuration to set DEV or PROD env

--- feedback-collector-nodejs-react/backend/config/keys.js
+++ feedback-collector-nodejs-react/backend/config/keys.js
@@ -1,8 +1,17 @@
-module.exports = {
-    googleClientId: "",
-    googleClientSecret: "",
-    mongoURI: '',
-    cookieKey: 'redacted'
+//Heroku production validation
+if(process.env.NODE_ENV === 'production') {
+    module.exports = {
+        googleClientId: process.env.GOOGLE_CLIENT_ID,
+        googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        mongoURI: process.env.MONGO_URI,
+        cookieKey: process.env.COOKIE_KEY
+    }
+} else {
+    module.exports = {
+        googleClientId: "",
+        googleClientSecret: "",
+        mongoURI: '',
+        cookieKey: 'redacted'
+    }
 }
-
 // Update googleClientId and googleClientSecret with Google Credentials, set MongoDB Atlas URI in mongoURI and rename this file to .keys.js
