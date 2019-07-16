e7721def7a01d13b05f36606238132c11c90ae32
ruas46 <guilhermeruas@hotmail.com>
2019-07-16 11:40:10 -0300

    refactored passport.js

--- feedback-collector-nodejs-react/backend/services/passport.js
+++ feedback-collector-nodejs-react/backend/services/passport.js
@@ -20,18 +20,16 @@ passport.use(
         callbackURL: '/auth/google/callback',
         proxy: true //To run in Heroku proxy
     },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    //We already have a record with the given profile ID
-                    done(null, existingUser)
-                } else {
-                    //We don't have a user record with this ID, make a new one
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user))
-                }
-            })
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id })
+
+        if (existingUser) {
+            //We already have a record with the given profile ID
+            done(null, existingUser)
+        } else {
+            //We don't have a user record with this ID, make a new one
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user)
+        }
     })
 )
