e35832b6802b2a0f3c6e0c1583ce6c0fc6e9b4c1
ruas46 <guilhermeruas@hotmail.com>
2019-07-16 17:29:22 -0300

    Added BrowserRouter and Route

--- feedback-collector-nodejs-react/frontend/src/App.js
+++ feedback-collector-nodejs-react/frontend/src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Dashboard from './components/Dashboard'
+import Landing from './components/Landing'
+import SurveyNew from './components/SurveyNew'
 
 const App = () => {
     return (
         <div>
-            Hi There!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/surveys" component={Dashboard} />
+                    <Route path="/surveys/new" component={SurveyNew} />
+                </div>
+            </BrowserRouter>
         </div>
     )
 }
