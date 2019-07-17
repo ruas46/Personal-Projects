5bbfa8ea10b0b73d69e9d62c9520d7948d9ba81d
ruas46 <guilhermeruas@hotmail.com>
2019-07-17 14:08:23 -0300

    Update Header style with materialize-css

--- feedback-collector-nodejs-react/frontend/src/components/Header.js
+++ feedback-collector-nodejs-react/frontend/src/components/Header.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux"
+import { Link } from "react-router-dom"
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+          case null:
+            return;
+          case false:
+            return (
+              <li>
+                <a href="/auth/google">Login with Google</a>
+              </li>
+            );
+          default:
+            return (
+              <li>
+                <a href="/api/logout">Logout</a>
+              </li>
+            );
+        }
+    }
+
     render() {
         return (
-            <div>Header</div>
+            <nav>
+                <div className="nav-weapper">
+                <Link
+                    to={this.props.auth ? "/surveys" : "/"}
+                    className="left brand-logo"
+                >
+                    Emaily
+                </Link>
+                <ul className="right">{this.renderContent()}</ul>
+                </div>
+            </nav>
         )
     }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header)
