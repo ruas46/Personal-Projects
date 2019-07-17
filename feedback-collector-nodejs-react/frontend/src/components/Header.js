b8c25be5b02ce124913baab0157f63c510017651
ruas46 <guilhermeruas@hotmail.com>
2019-07-17 14:12:15 -0300

    Fix package.json

--- feedback-collector-nodejs-react/frontend/src/components/Header.js
+++ feedback-collector-nodejs-react/frontend/src/components/Header.js
@@ -30,7 +30,7 @@ class Header extends Component {
                     to={this.props.auth ? "/surveys" : "/"}
                     className="left brand-logo"
                 >
-                    Emaily
+                    Feedback-Collector
                 </Link>
                 <ul className="right">{this.renderContent()}</ul>
                 </div>
