c15c8a1cefdb047194ac88d9740760268c0ebdda
ruas46 <guilhermeruas@hotmail.com>
2019-07-17 14:07:52 -0300

    Added Axios, fetch_user and redux

--- feedback-collector-nodejs-react/frontend/src/App.js
+++ feedback-collector-nodejs-react/frontend/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import * as actions from './actions'
 
 import Header from './components/Header'
 import Dashboard from './components/Dashboard'
 import Landing from './components/Landing'
 import SurveyNew from './components/SurveyNew'
 
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    )
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser()
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        )
+    }
 }
 
-export default App
+export default connect(null, actions)(App)
