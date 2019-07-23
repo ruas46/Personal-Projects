48ea4c76d7000464dbe0b0c633acef5de6aa6188
ruas46 <guilhermeruas@hotmail.com>
2019-07-23 19:13:15 -0300

    Refactor survey fields const to a new file

--- feedback-collector-nodejs-react/frontend/src/components/surveys/SurveyForm.js
+++ feedback-collector-nodejs-react/frontend/src/components/surveys/SurveyForm.js
@@ -4,24 +4,18 @@ import { reduxForm, Field } from 'redux-form'
 import { Link } from 'react-router-dom'
 import SurveyField from './SurveyField'
 import validateEmails from '../../utils/validateEmails'
-
-const FIELDS = [
-    { label: "Survey Title", name: "title" },
-    { label: "Survey Line", name: "subject" },
-    { label: "Email Body", name: "body" },
-    { label: "Recipient List", name: "emails" }
-]
+import formFields from './formFields'
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, field => {
+        return _.map(formFields, field => {
             return <Field key={field.name} component={SurveyField} type="text" label={field.label} name={field.name} />
         })
     }
     render() {
         return (
             <div>
-                <form onSubmit={this.props.handleSubmit(v=> console.log(v))}>
+                <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                     {this.renderFields()}
                     <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
                     <button type="submit" className="teal btn-flat right white-text">
@@ -39,7 +33,7 @@ function validate(values) {
 
     errors.emails = validateEmails(values.emails || '')
 
-    _.each(FIELDS, ({ name }) => {
+    _.each(formFields, ({ name }) => {
         if (!values[name]) {
             errors[name] = `You must provide a ${name}`
         }
@@ -50,5 +44,6 @@ function validate(values) {
 
 export default reduxForm({
     validate: validate,
-    form: 'surveyForm'
+    form: 'surveyForm',
+    destroyOnUnmount: false
 })(SurveyForm)
