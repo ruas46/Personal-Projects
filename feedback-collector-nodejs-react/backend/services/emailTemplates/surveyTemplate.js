9deab6375d1ee106b810d6b1422e4dc74f9ef3e1
ruas46 <guilhermeruas@hotmail.com>
2019-07-25 17:31:57 -0300

    Added Route to handle with sendgrid webhook

--- feedback-collector-nodejs-react/backend/services/emailTemplates/surveyTemplate.js
+++ feedback-collector-nodejs-react/backend/services/emailTemplates/surveyTemplate.js
@@ -7,10 +7,10 @@ module.exports = (survey) => {
                     <p>Please answer the following question:</p>
                     <p>${survey.body}</p>
                     <div>
-                        <a href="http://localhost:3000/api/surveys/thanks">Yes</a>
+                        <a href="http://localhost:3000/api/surveys/${survey.id}/yes">Yes</a>
                     </div>
                     <div>
-                        <a href="http://localhost:3000/api/surveys/thanks">No</a>
+                        <a href="http://localhost:3000/api/surveys/${survey.id}/no">No</a>
                     </div>
                 </div>
             </body>
