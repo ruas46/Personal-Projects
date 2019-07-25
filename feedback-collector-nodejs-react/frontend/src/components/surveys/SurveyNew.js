ce2cd8ea8001f1067ba39f240f64b115e39e773c
ruas46 <guilhermeruas@hotmail.com>
2019-07-25 11:30:02 -0300

    Added Survey Form Review and send function to backend

--- feedback-collector-nodejs-react/frontend/src/components/surveys/SurveyNew.js
+++ feedback-collector-nodejs-react/frontend/src/components/surveys/SurveyNew.js
@@ -1,15 +1,33 @@
 // SurveyNew shows SurveyFrom and SurveyFormReview
 import React, { Component } from 'react'
+import { reduxForm } from 'redux-form'
 import SurveyForm from './SurveyForm'
+import SurveyFormReview from './SurveyFormReview'
 
 class SurveyNew extends Component {
+    state = { showFormReview: false }
+
+    renderContent() {
+        if (this.state.showFormReview === true) {
+            return <SurveyFormReview 
+                onCancel={() => this.setState({ showFormReview: false })}
+            />
+        } else {
+            return <SurveyForm
+                onSurveySubmit={() => this.setState({ showFormReview: true })}
+            />
+        }
+    }
+
     render() {
         return (
             <div className="container">
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         )
     }
 }
 
-export default SurveyNew
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyNew)
