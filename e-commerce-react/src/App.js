21373f6fd86b0d8275603af7cd4421b07408bec7
ruas46 <guilhermeruas@hotmail.com>
2019-08-04 22:37:56 -0300

    Added SignIn page with FormInput and CustomButton

--- e-commerce-react/src/App.js
+++ e-commerce-react/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/HomePage/HomePage'
 import ShopPage from './pages/ShopPage/ShopPage'
+import SingInAndUp from './pages/SignInAndUp/SignInAndUp'
 import Header from './components/Header/Header'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SingInAndUp} />
       </Switch>
     </div>
   );
---
+++ e-commerce-react/src/pages/SignInAndUp/SignInAndUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './SignInAndUp.scss'
+import SignIn from '../../components/SignIn/SignIn'
+
+const SingInAndUp = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+)
+
+export default SingInAndUp
