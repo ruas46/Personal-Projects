f9dee463346978b09f5a85837f68974369d5a817
ruas46 <guilhermeruas@hotmail.com>
2019-08-05 18:14:32 -0300

    Added SignIn function to Application

--- e-commerce-react/src/components/SignIn/SignIn.jsx
+++ e-commerce-react/src/components/SignIn/SignIn.jsx
@@ -4,7 +4,7 @@ import './SignIn.scss'
 import FormInput from '../FormInput/FormInput'
 import CustomButton from '../CustomButton/CustomButton'
 
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -16,10 +16,17 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault()
 
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' })
+        } catch (error){
+            console.log(error)
+        }
     }
 
     handleChange = event => {
