f9af0e37e67b29c8162f02ccead5fc3a76168645
ruas46 <guilhermeruas@hotmail.com>
2019-08-07 10:43:21 -0300

    Added base frontend files

---
+++ tinder-demo-nodejs-react/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <img src={logo} alt='Tindev' />
+  );
+}
+
+export default App;
