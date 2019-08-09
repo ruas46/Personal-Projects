97636ca7c6fbcad4e653f01dedb92ee9e5a9cade
ruas46 <guilhermeruas@hotmail.com>
2019-08-09 18:08:58 -0300

    Added socket connection in backend with socket.io to send Devs information in Match likes

--- tinder-demo-nodejs-react/backend/src/server.js
+++ tinder-demo-nodejs-react/backend/src/server.js
@@ -4,15 +4,31 @@ const cors = require('cors')
 
 const routes = require('./routes')
 
-const server = express()
+const httpServer = express()
+const server = require('http').Server(httpServer)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query
+    connectedUsers[user] = socket.id
+})
 
 mongoose.connect(
     'mongodb+srv://admin:<password>@cluster0-1mgfe.mongodb.net/tinder?retryWrites=true&w=majority',
     { useNewUrlParser: true }
 )
 
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+httpServer.use((req, res, next) => {
+    req.io = io
+    req.connectedUsers = connectedUsers
+    
+    return next()
+})
+
+httpServer.use(cors())
+httpServer.use(express.json())
+httpServer.use(routes)
 
 server.listen(3333)
