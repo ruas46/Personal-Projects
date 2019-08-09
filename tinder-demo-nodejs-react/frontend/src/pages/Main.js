b20ce5665585143fd4c3371df32825d9666db945
ruas46 <guilhermeruas@hotmail.com>
2019-08-09 18:10:03 -0300

    Added socket connection in frontend with socket.io-client to get Devs information in Match likes and show a popup for them

--- tinder-demo-nodejs-react/frontend/src/pages/Main.js
+++ tinder-demo-nodejs-react/frontend/src/pages/Main.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Main.css'
+import io from 'socket.io-client'
 
 import api from '../services/api'
 
 import logo from '../assets/logo.svg'
 import like from '../assets/like.svg'
 import dislike from '../assets/dislike.svg'
+import itsamatch from '../assets/itsamatch.png'
 
 export default function Main({ match }) {
     const [users, setUsers] = useState([])
+    const [matchDev, setMatchDev] = useState(null)
 
     useEffect(() => {
         async function loadUsers() {
@@ -23,6 +26,16 @@ export default function Main({ match }) {
         }
 
         loadUsers()
+    }, [match.params.id])
+
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id }
+        })
+
+        socket.on('match', dev => {
+            setMatchDev(dev)
+        })
     }, [match.params.id])
 
     async function handleDislike(id) {
@@ -71,6 +84,16 @@ export default function Main({ match }) {
                     <div className="empty">You don't have more devs :(</div>
                 )
             }
+
+            { matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match" />
+                    <img src={matchDev.avatar} className="avatar" alt={matchDev.name} />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+                    <button type="button" onClick={() => setMatchDev(null)}>Close</button>
+                </div>
+            ) }
 
         </div>
     )
