f5cae4a0a5368344012325f90aea2b56f7ce9a79
ruas46 <guilhermeruas@hotmail.com>
2019-08-10 18:14:40 -0300

    Added Cart Icon component in to Header

--- e-commerce-react/src/components/Header/Header.jsx
+++ e-commerce-react/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import  { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../CartIcon/CartIcon'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
     </div>
 )
