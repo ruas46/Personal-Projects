87e1fb29d914882152335464f20b92b3b793b803
ruas46 <guilhermeruas@hotmail.com>
2019-08-10 18:39:51 -0300

    Added Cart Dropdown component in to Header

--- e-commerce-react/src/components/Header/Header.jsx
+++ e-commerce-react/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 
 import  { auth } from '../../firebase/firebase.utils'
 import CartIcon from '../CartIcon/CartIcon'
+import CartDropdown from '../CartDropdown/CartDropdown'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -29,6 +30,7 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 
