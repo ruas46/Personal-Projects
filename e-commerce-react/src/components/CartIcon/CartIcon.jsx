3ae1c3df1d72fd63d0fb808d80441fa183ce036c
ruas46 <guilhermeruas@hotmail.com>
2019-08-11 17:53:52 -0300

    Update cartIcon to show quantity inside cart dynamically with redux

--- e-commerce-react/src/components/CartIcon/CartIcon.jsx
+++ e-commerce-react/src/components/CartIcon/CartIcon.jsx
@@ -5,10 +5,10 @@ import { toggleCartHidden } from '../../redux/cart/cartActions'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import './CartIcon.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
@@ -16,4 +16,10 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+const mapStateToProps = ({ cart: { cartItems }}) => ({
+    itemCount: cartItems.reduce(
+        (totalQuanty, cartItem) => totalQuanty + cartItem.quantity
+    , 0)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
