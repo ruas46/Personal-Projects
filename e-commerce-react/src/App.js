da912ad95654848da1aead5ec287b1a6d5518791
ruas46 <guilhermeruas@hotmail.com>
2019-08-12 13:43:05 -0300

    Added Checkout Page

--- e-commerce-react/src/App.js
+++ e-commerce-react/src/App.js
@@ -7,7 +7,9 @@ import { createStructuredSelector } from 'reselect'
 import HomePage from './pages/HomePage/HomePage'
 import ShopPage from './pages/ShopPage/ShopPage'
 import SingInAndUp from './pages/SignInAndUp/SignInAndUp'
+import CheckoutPage from './pages/Checkout/Checkout'
 import Header from './components/Header/Header'
+
 import { auth, createUserProfile } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/userActions'
 import { selectCurrentUser } from './redux/user/userSelectors'
@@ -45,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact path='/signin'
             render={
