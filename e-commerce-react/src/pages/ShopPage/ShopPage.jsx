64d09caa98908f3334403c3e496d02039afbdf6a
ruas46 <guilhermeruas@hotmail.com>
2019-08-14 10:44:09 -0300

    Added nested route in shopPage

--- e-commerce-react/src/pages/ShopPage/ShopPage.jsx
+++ e-commerce-react/src/pages/ShopPage/ShopPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview'
+import CollectionPage from '../Collection/Collection'
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
     <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )
 
